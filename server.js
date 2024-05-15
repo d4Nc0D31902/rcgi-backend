2f9b67123344833eb8e0ee219a6924886416e550
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-05-15 11:02:57 +0800

    origin: "https://rcgi-frontend.vercel.app",

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://rcgi-frontend.vercel.app"],
+    // origin: ["http://localhost:3000", "https://rcgi-frontend.vercel.app"],
+    origin: "https://rcgi-frontend.vercel.app",
     credentials: true,
   },
 });
