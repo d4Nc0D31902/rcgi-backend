9e437a41d3cd60dd5f9c9905987e9dd70ef41a77
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-05-23 15:01:44 +0800

    ADDED RENDER CODE

--- server.js
+++ server.js
@@ -19,3 +19,55 @@ app.listen(process.env.PORT, () => {
     `server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
 });
+
+// RENDER
+
+// const app = require("./app");
+// const connectDatabase = require("./config/database");
+// const path = require("path");
+// const cloudinary = require("cloudinary");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const cors = require("cors");
+// //TRUE SOCKET USE THIS ^
+
+// require("dotenv").config({ path: "./config/.env" });
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// connectDatabase();
+// console.log(process.env.DATABASE);
+
+// if (process.env.NODE_ENV !== "PRODUCTION")
+//   require("dotenv").config({ path: "backend/config/.env" });
+
+
+// //TRUE SOCKET USE THIS
+// const server = http.createServer(app);
+
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     // origin: "https://rcgi-frontend.onrender.com",
+//     credentials: true,
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//   });
+// });
+
+// global.io = io;
+
+// server.listen(process.env.PORT, () => {
+//   console.log(
+//     `Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+//   );
+// });
